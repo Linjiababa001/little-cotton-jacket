2be39f7d0bd0c967f62cb640ab72266942785f0d
Linjiababa001 <406938493@qq.com>
2026-07-29 11:23:35 +0800

    Fix: registerSW.js

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/little-cotton-jacket/sw.js', { scope: '/little-cotton-jacket/' })})}
